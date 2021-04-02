6519deef49163907ab688304dbe253cf493b82c2
VytautasKaz <vytautas.kazakevicius2@gmail.com>
2021-04-02 17:46:49 +0300

    routing

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require_once "./bootstrap.php";
+
+$request = $_SERVER['REQUEST_URI'];
+
+$root = '/cms';
+
+switch ($request) {
+    case $root . '/':
+        require __DIR__ . '/src/views/home.php';
+        break;
+    case $root:
+        require __DIR__ . '/src/views/home.php';
+        break;
+    case $root . '/admin':
+        require __DIR__ . '/src/views/admin.php';
+        break;
+    default:
+        http_response_code(404);
+        require __DIR__ . '/src/views/err404.php';
+        break;
+}
